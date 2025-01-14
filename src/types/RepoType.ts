aa272dfd457645129cf8eb0b935e0c95f626cf0a
KatrinBnk <yekaterinabanko@gmail.com>
2025-01-14 21:39:53 +0300

    Defining the data type for a repo

---
+++ src/types/RepoType.ts
@@ -0,0 +1,12 @@
+export interface Repo {
+    id: number;
+    name: string; // Название
+    description: string; // Описание
+    owner: {
+        login: string; // Ник владельца
+        type: string; // Тип владельца (User или Organization)
+    };
+    html_url: string; // Ссылка
+    stargazers_count: number; // Звезды на репо
+    topics: string[]; // Темы
+}
